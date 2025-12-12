830ea3932458d3ca12e3ddde086bc1de079caa23
Patryk Palej <patrykpalej@proton.me>
2025-12-12 14:32:03 +0100

    Implement basic main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+from characters.professions import Warrior, Wizard, Priest
+from characters.races import Human, Elf, Orc
+
+
+team_composition = {Warrior: 2, Wizard: 2, Priest: 1}
+available_races = (Human, Elf, Orc)
+tasks_count = 5
