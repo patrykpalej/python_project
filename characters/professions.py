ad65aedd12ac99f3ae3d4527c80f335aeecf7b2e
Patryk Palej <patrykpalej@proton.me>
2025-12-12 12:03:54 +0100

    Character professions classes

--- characters/professions.py
+++ characters/professions.py
@@ -1,7 +1,11 @@
+from utils import dice_roll, expected_dice_roll
+
+
 class Character:
-    max_hp = 10.0
+    max_hp = 10
 
     def __init__(self, name, race):
+
         self.name = name
         self.race = race
         self.hp = 8.0
@@ -12,11 +16,9 @@ class Character:
     def to_json(self):
         return self.__dict__
 
-    def take_damage(self, amount):
-        self.hp = max(0, self.hp - max([0, amount]))
-
-    def heal(self, amount):
-        self.hp = min(self.hp + amount, self.max_hp)
+    @property
+    def is_alice(self):
+        return self.hp > 0
 
     def __str__(self):
         return self.name
@@ -24,6 +26,74 @@ class Character:
     def __repr__(self):
         return f"{self.name} - {self.race} {self.__class__.__name__}"
 
-    @property
-    def is_alive(self):
-        return self.hp > 0
+    def take_damage(self, damage_value):
+        self.hp = max(0, self.hp - damage_value)
+
+    def heal(self, heal_value):
+        self.hp = min(self.max_hp, self.hp + heal_value)
+
+
+class Warrior(Character):
+    def __init__(self, name, race):
+        super().__init__(name, race)
+
+        self.strength += 2
+        self.intelligence -= 1
+
+    def expected_contribution(self, task_type):
+        return (self.strength + expected_dice_roll
+                if task_type == "combat" else 2 + expected_dice_roll)
+
+    def contribute(self, task_type):
+        return (self.strength + dice_roll()
+                if task_type == "combat" else 2 + dice_roll())
+
+
+class Wizard(Character):
+    def __init__(self, name, race):
+        super().__init__(name, race)
+
+        self.strength -= 1
+        self.intelligence += 2
+        self.faith += 1
+        self.mana = 5.0
+
+    def expected_contribution(self, task_type):
+        return (self.mana // 2 + self.intelligence + expected_dice_roll
+                if task_type == "magic" else 2 + expected_dice_roll)
+
+    def contribute(self, task_type):
+        if task_type == "magic":
+            result = self.mana // 2 + self.intelligence + dice_roll()
+            self.__spend_mana()
+        else:
+            result = 2 + dice_roll()
+        return result
+
+    def __spend_mana(self):
+        self.mana = max([0.0, self.mana-1])
+
+
+class Priest(Character):
+    def __init__(self, name, race):
+        super().__init__(name, race)
+
+        self.strength -= 2
+        self.intelligence += 1
+        self.faith += 3
+
+        self.mana = 5.0
+
+    def expected_contribution(self, task_type):
+        return (self.faith + expected_dice_roll
+                if task_type in ("holly", "support") else 1 + expected_dice_roll)
+
+    def contribute(self, task_type):
+        return (self.faith + dice_roll()
+                if task_type in ("holly", "support") else 1 + dice_roll())
+
+    def heal_ally(self, team_member):
+        if self.mana > 0:
+            heal_amount = self.faith / 5
+            team_member.heal(heal_amount)
+            self.mana = max([0, self.mana-(heal_amount * 0.1)])
---
+++ utils.py
@@ -0,0 +1,8 @@
+import random
+
+
+expected_dice_roll = 3.5
+
+
+def dice_roll(k=6):
+    return random.randint(1, k)
